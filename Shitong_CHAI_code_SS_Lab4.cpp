9ed80bd5fc44ad7f90dc3c88a23df635f44f10f7
柴士童 <836275709@qq.com>
2019-12-13 03:44:37 +0100

    Update Shitong_CHAI_code_SS_Lab4.cpp

--- Shitong_CHAI_code_SS_Lab4.cpp
+++ Shitong_CHAI_code_SS_Lab4.cpp
@@ -10,19 +10,44 @@
 int main()
 {
     list<Rabbit*> pop;
-    for(int i=0;i<5;i++)
+    for(int i=0;i<50;i++)
     {
-        Rabbit *r = new MaleRabbit(i);
+        Rabbit *r = new FemaleRabbit();
         pop.push_back(r);
     }
-    for(int i=5;i<9;i++)
+    for(int i=50;i<90;i++)
     {
-        Rabbit *r = new FemaleRabbit(i);
+        Rabbit *r = new MaleRabbit();
         pop.push_back(r);
     }
     for(list<Rabbit*>::iterator i=pop.begin(); i!=pop.end();++i)
     {
         (*i)->print();
     }
+    cout<<endl;
+    //list<Rabbit*>::iterator old_i;
+    
+    for(int j=0;j<400;j++)
+    {
+        list<Rabbit*>::iterator i=pop.begin();
+        list<Rabbit*>::iterator end=pop.end();
+        while(i != end)
+        {
+            
+            bool is_dead = (*i)->one_day_life(pop);
+            (*i)->print();
+            // cout<<(is_dead?"is dead":"is alive")<<endl;
+            if(is_dead)
+                pop.erase(i++);
+            else
+                i++;
+        }
+    }
+    
+    // cout<<pop.size()<<endl;
+    // for(list<Rabbit*>::iterator i=pop.begin(); i!=pop.end();++i)
+    // {
+        // (*i)->print();
+    // }
 
 }
