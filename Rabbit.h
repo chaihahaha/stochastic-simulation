f1788875dc67ca84eaa63b656adf0ba4f96221c2
柴士童 <836275709@qq.com>
2019-12-14 22:14:11 +0100

    Update Rabbit.h
    Removing redundant methods in FemaleRabbit, 1 day simu to n days simu

--- Rabbit.h
+++ Rabbit.h
@@ -15,21 +15,6 @@
 #define YEAR 365
 #define MONTH 30
 using namespace std;
-double uniform(double a, double b)
-{
-    /*
-     * :argument 
-     *       a,b are the endpoints of an interval [a,b]. 
-     * :return  
-     *       A random number in the interval [a,b].
-     * :usage
-     *       To generate a uniformly distributed 
-     *       random number.
-     */
-
-    return (b - a) * genrand_real2() + a;
-}
-
 
 class Rabbit
 {
@@ -39,6 +24,7 @@ class Rabbit
         int age;
         int id;
         int mature_age;
+        double female_ratio=0.5;
         bool is_female;
         bool is_dead;
         Rabbit():age(0), is_female(false), is_dead(false)
@@ -53,11 +39,11 @@ class Rabbit
             else
                 return true;
         }
-        virtual bool one_day_life(list<Rabbit*> &population){}
+        virtual bool n_days_life(list<Rabbit*> &population, int step){}
         
-        bool die()
+        bool die(int step)
         {
-            if(genrand_real2()<survival_rate())
+            if(genrand_real2()<survival_rate(step))
                 return false;
             else
             {
@@ -69,16 +55,19 @@ class Rabbit
     private:
         double survival_rate_per_year()
         {
+            
             if(!is_mature())
                 return 0.2;
-            else if(is_mature() && age <= 10 * YEAR)
+            else if(age <= 2 * YEAR)
                 return 0.5;
             else
-                return max(0.5 - (age/YEAR - 10)*0.1, 0.0);
+            {
+                return max(0.5 - (age/YEAR - 2)*0.25, 0.0);
+            }
         }
-        double survival_rate()
+        double survival_rate(int step)
         {
-            return pow(survival_rate_per_year(), 1.0/365);
+            return pow(survival_rate_per_year()*0.4, 1.0/YEAR*(double)step);
         }
         
 };
@@ -93,13 +82,13 @@ class MaleRabbit: public Rabbit
             is_female=false;
             mature_age = (genrand_int31()%3 + 5)*MONTH;
         }
-        bool one_day_life(list<Rabbit*> &population)
+        bool n_days_life(list<Rabbit*> &population, int step)
         {
-            die();
+            die(step);
             if(!is_dead)
             {
                 bool not_mature=!is_mature();
-                age++;
+                age+=step;
                 bool mature = is_mature();
                 if(not_mature && mature)
                     mature_male_counter++;
@@ -126,32 +115,27 @@ class FemaleRabbit: public Rabbit
             pregnant_cycle = genrand_int31()%10 + 27;
             lactational_amenorrhea = genrand_int31()%3;
         }
-        bool one_day_life(list<Rabbit*> &population)
+        bool n_days_life(list<Rabbit*> &population, int step)
         {
             // return true if dead
             
             if(!is_dead)
             {
                 
-                age++;
-                if(!is_pregnant() && is_mature())
-                    days_after_parturition++;
-                if(is_pregnant())
-                    days_after_pregnant++;
-                get_pregnant();
+                age+=step;
+                if(is_mature())
+                    days_after_pregnant+=step;
                 give_birth(population);
             }
-            return die();
+            return die(step);
             
         }
         void print() 
         {
-            cout<<"Age: "<<age<<",\tID: "<<id<<",\t"<<(is_female?"Female":"Male")<<",\t"<<(is_dead?"Dead":"Alive")<<",\t"<<(is_mature()?"Is Mature":"Not Mature")<<", days after parturition:"<<days_after_parturition<<",\tdays after pregnant:"<<days_after_pregnant<<",\t"<<(is_pregnant()?"Pregnant":"Not Pregnant")<<endl;
+            cout<<"Age: "<<age<<",\tID: "<<id<<",\t"<<(is_female?"Female":"Male")<<",\t"<<(is_dead?"Dead":"Alive")<<",\t"<<(is_mature()?"Is Mature":"Not Mature")<<",\tdays after pregnant:"<<days_after_pregnant<<endl;
         }
     private:
-        int days_after_parturition=0;
         int days_after_pregnant=0;
-        bool _is_pregnant=false;
         int pregnant_cycle; //least time of pregnancy needed before giving birth
         int lactational_amenorrhea; //least time of Lactational Amenorrhea
         bool female_birth(double gender_ratio_female)
@@ -168,52 +152,28 @@ class FemaleRabbit: public Rabbit
             return (int)(pow(genrand_real2(),2)/(1.0/6))+1;
         }
         
-        bool is_pregnant()
-        {
-            return _is_pregnant; //Amenorrhea
-        }
-        bool can_give_birth()
-        {
-            //not in Lactational Amenorrhea, not Immature, fertile
-            if((is_pregnant()) && is_mature() && days_after_pregnant >= pregnant_cycle)
-                return true; 
-            else
-                return false;
-        }
-        bool can_get_pregnant()
-        {
-            //not in Lactational Amenorrhea, not Immature, fertile
-            if((!is_pregnant()) && is_mature() && days_after_parturition >= lactational_amenorrhea && mature_male_counter>0)
-                return true; 
-            else
-                return false;
-        }
-        void get_pregnant()
-        {
-            if(can_get_pregnant())
-            {
-                days_after_pregnant=0;
-                _is_pregnant = true;
-            }
-        }
+
         void give_birth(list<Rabbit*> &population)
         {
-            if(can_give_birth())
+            if(is_mature() && mature_male_counter>0)
             {
-                //cout<<"giving birth"<<endl;
                 //can give birth if not in amenorrhea
-                int number_of_birth = fertility();
+                int cycles = days_after_pregnant/(pregnant_cycle + lactational_amenorrhea);
+                int number_of_birth = 0;
+                for(int i=0; i<cycles; i++)
+                {
+                    number_of_birth += fertility();
+                }
+                days_after_pregnant -= cycles*(pregnant_cycle + lactational_amenorrhea);
                 for(int i=0; i<number_of_birth; i++)
                 {
                     Rabbit *baby;
-                    if(female_birth(0.5))
+                    if(female_birth(female_ratio))
                         baby = new FemaleRabbit();
                     else
                         baby = new MaleRabbit();
                     population.push_front(baby);
                 }
-                days_after_parturition=0;
-                _is_pregnant=false;
             }
             
             return;
